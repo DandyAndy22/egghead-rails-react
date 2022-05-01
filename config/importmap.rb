3a6a10fb9b02053f2d106b841126bd11a5f60613
dandyandy22 <andrasm96@gmail.com>
2022-05-01 13:07:41 -0700

    Components Added

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/components", under: "components"
+pin "htm", to: "https://ga.jspm.io/npm:htm@3.1.1/dist/htm.module.js"
+pin "react", to: "https://ga.jspm.io/npm:react@18.1.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.1.0/index.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.24/nodelibs/browser/process-production.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.22.0/index.js"
